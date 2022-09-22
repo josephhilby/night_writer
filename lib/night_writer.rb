45fe03b7cf9a4d8759164fa03cc02cfb05109504
Joseph Hilby <joseph.hilby@gmail.com>
2022-09-22 17:09:02 -0500

    Add night_writer I/0

---
+++ lib/night_writer.rb
@@ -0,0 +1,12 @@
+message_file = File.open(ARGV[0], 'r')
+message = message_file.read
+count = message.length
+
+cypher = Cypher.new(message)
+cyphered_msg = cypher.encode
+
+braille_file = File.open(ARGV[1], 'w')
+braille_file.write(cyphered_msg)
+braille_file.close
+
+puts "Created #{ARGV[1]} containing #{count} characters"

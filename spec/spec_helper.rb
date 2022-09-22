28a49e56d1ecc720c13bfa4558ca9874b41c52fd
Joseph Hilby <joseph.hilby@gmail.com>
2022-09-22 15:06:09 -0500

    Initial commit

---
+++ spec/spec_helper.rb
@@ -0,0 +1,11 @@
+require "simplecov"
+SimpleCov.start
+
+# require_relative 'activity_spec'
+# require_relative
+# require_relative
+
+
+RSpec.configure do |config|
+  config.formatter = :documentation
+end

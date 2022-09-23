d95c87da9c535d2ba24cfdb4ed52c9a8347a1c77
Joseph Hilby <joseph.hilby@gmail.com>
2022-09-23 14:09:28 -0500

    Fix bletchley_park_computer_spec

--- spec/bletchley_park_computer_spec.rb
+++ spec/bletchley_park_computer_spec.rb
@@ -34,18 +34,7 @@ RSpec.describe BletchleyParkComputer do
 
   context "Methods" do
     it "#file_split" do
-      # expect(@bombe.line_split).to eq([["O.", "OO", ".."]])
-      # expect(@bombe.line_split).to eq([[["O.", "OO", ".."], ["O.", "OO", ".."], ["O.", "OO", ".."]]])
       expect(@bombe.file_split).to eq(["O.O.O.O.O.", "OO.OO.O..O", "....O.O.O."])
-      # expect(@bombe.line_split).to eq([["O.O.O.O.O.  .OO.O.O.OO", "OO.OO.O..O  OO.OOOO..O", "....O.O.O.  .OO.O.O..."]])
-
-      # [file[line_1.split("\n")[line_1.1],
-      #                         [line_1.2],
-      #                         [line_1.3]],
-      #      [line_2            [line_2.1],
-      #                         [line_2.2],
-      #                         [line_2.3]]
-      # ]
     end
 
     it "#line_join" do

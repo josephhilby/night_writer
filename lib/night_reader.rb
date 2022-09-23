7497ee3bf69e222d9d6414cbbe032a572b4b77e7
Joseph Hilby <joseph.hilby@gmail.com>
2022-09-23 15:00:10 -0500

    WIP night_reader

---
+++ lib/night_reader.rb
@@ -0,0 +1,17 @@
+require './lib/black_fill'
+require './lib/bletchley_park_computer'
+
+message_file = File.open(ARGV[0], 'r')
+message = message_file.read
+
+b_fill = BlackFill.new
+bombe = BletchleyParkComputer.new(message, b_fill.black_fill)
+decoded_msg = bombe.letter_decode
+
+
+braille_file = File.open(ARGV[1], 'w')
+count = decoded_msg.length
+braille_file.write(decoded_msg)
+braille_file.close
+
+puts "Created #{ARGV[1]} containing #{count} characters"

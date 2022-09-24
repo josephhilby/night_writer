938b28a6cbeb11a738bd55e9e75c2b09e95042d9
Joseph Hilby <joseph.hilby@gmail.com>
2022-09-24 08:58:09 -0500

    Add two stubs to bletchley_park_computer_spec

--- spec/bletchley_park_computer_spec.rb
+++ spec/bletchley_park_computer_spec.rb
@@ -1,23 +1,20 @@
 require 'rspec'
-require './lib/red_fill'
 require './lib/black_fill'
-require './lib/cypher'
 require './lib/formatter'
 require './lib/bletchley_park_computer'
 
 RSpec.describe BletchleyParkComputer do
   before(:each) do
-    r_fill = RedFill.new
     b_fill = BlackFill.new
-    cypher = Cypher.new('hello', r_fill.red_fill_1, r_fill.red_fill_2, r_fill.red_fill_3)
-    cypher.encode
-
-    formatter = Formatter.new(cypher.encoded_line_1,
-                              cypher.encoded_line_2,
-                              cypher.encoded_line_3)
-    formatter.merge
+    formatter = Formatter.new(["O.O.O.O.O."],
+                              ["OO.OO.O..O"],
+                              ["....O.O.O."])
+    allow(b_fill).to receive(:black_fill).and_return({ 'e' => ['O.', '.O', '..'],
+                                                       'h' => ["O.", "OO", ".."],
+                                                       'l' => ['O.', 'O.', 'O.'],
+                                                       'o' => ['O.', '.O', 'O.'] })
+    allow(formatter).to receive(:encrypted_message).and_return("O.O.O.O.O.\nOO.OO.O..O\n....O.O.O.")
     incoming_msg = formatter.encrypted_message
-    #replace above with stub
     @bombe = BletchleyParkComputer.new(incoming_msg, b_fill.black_fill)
   end
 
